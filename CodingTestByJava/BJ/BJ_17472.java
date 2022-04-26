611cd026dc0b9b0b8f40537422e215af079b7f08
Park Sungchul <try615@naver.com>
2022-04-26 16:43:02 +0900

    백준 17472 재풀이

--- CodingTestByJava/BJ/BJ_17472.java
+++ CodingTestByJava/BJ/BJ_17472.java
@@ -1,37 +1,35 @@
-import java.io.*;
+package day_220426;
+
 import java.util.*;
+import java.io.*;
 
-
-// 백준 17472번. 다리 만들기 2.
-// BFS로 섬을 넘버링 후, 모든 위치에서 사방 탐색하여 간선 정보 획득. 크루스칼.
 public class BJ_17472 {
-	static int N, M, landIdx, result;
+	static int N, M, result, landIdx, landConnect;
 	static int[][] field;
 	static boolean[][] visited;
-	static int landConnect;
-	
 	static int[] dr = {0, 1, 0, -1};
 	static int[] dc = {1, 0, -1, 0};
+	static PriorityQueue<Bridge> pq;
+	static int[] check;
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		landIdx = 1;
+		
 		result = 0;
-		
 		field = new int[N][M];
-		visited = new boolean[N][M];
-		
 		for(int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
+			
 			for(int j=0; j<M; j++) {
 				field[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
+		landIdx = 1;
+		visited = new boolean[N][M];
 		for(int i=0; i<N; i++) {
 			for(int j=0; j<M; j++) {
 				if(field[i][j] == 1 && !visited[i][j]) {
@@ -40,54 +38,27 @@ public class BJ_17472 {
 			}
 		}
 		
-		int[] parents = new int[landIdx];
-		for(int i=1; i<landIdx; i++) {
-			parents[i] = i;
-		}
-		
-		List<Edge> list = new ArrayList<Edge>();
-		
+		landConnect = landIdx-2;
+		pq = new PriorityQueue<>();
 		for(int i=0; i<N; i++) {
 			for(int j=0; j<M; j++) {
-				if(field[i][j] != 0) {
-					int from = field[i][j];
-					for(int dir=0; dir<4; dir++) {
-						int ar = i;
-						int ac = j;
-						int lenCount = 0;
-						
-						while(true) {
-							ar += dr[dir];
-							ac += dc[dir];
-							
-							if(ar < 0 || ar >= N || ac < 0 || ac >= M) {
-								break;
-							}
-							
-							if(field[ar][ac] != 0) {
-								if(field[ar][ac] != from && lenCount > 1) {
-									int to = field[ar][ac];
-									list.add(new Edge(from, to, lenCount));
-								}
-								break;
-							}
-							lenCount++;
-						}
-					}
+				if(field[i][j] > 0) {
+					makeBridge(field[i][j], new int[] {i, j});
 				}
 			}
 		}
-		Collections.sort(list);
 		
-		landConnect = landIdx-2;
-		for(Edge e : list) {
-			int parentFrom = find(parents, e.from);
-			int parentTo = find(parents, e.to);
+		check = new int[landIdx];
+		for(int i=1; i<landIdx; i++) {
+			check[i] = i;
+		}
+		
+		while(!pq.isEmpty()) {
+			Bridge current = pq.poll();
 			
-			if(parentFrom != parentTo) {
-				union(parents, parentFrom, parentTo);
-				result += e.weight;
-				landConnect--;
+			if(union(current.start, current.dest)) {
+				result += current.len;
+				landConnect -= 1;
 			}
 			
 			if(landConnect <= 0) {
@@ -95,81 +66,117 @@ public class BJ_17472 {
 			}
 		}
 		
-		if(landConnect <= 0) {
-			System.out.println(result);
-		}else {
-			System.out.println(-1);
+		if(landConnect > 0) {
+			result = -1;
 		}
+		
+//		for(int i=0; i<N; i++) {
+//			System.out.println(Arrays.toString(field[i]));
+//		}
+//		
+//		System.out.println(Arrays.toString(check));
+		
+		System.out.println(result);
 		br.close();
 	}
 	
+	public static int find(int child) {
+		if(check[child] != child) {
+			check[child] = find(check[child]);
+		}
+		return check[child];
+	}
 	
-	private static int find(int[] parents, int node) {
-		if(parents[node] != node) {
-			parents[node] = find(parents, parents[node]);
-		}
-		return parents[node];
-	}
-
-
-
-	private static void union(int[] parents, int parentFrom, int parentTo) {
-		if(parentFrom < parentTo) {
-			parents[parentTo] = parentFrom;
-		}else {
-			parents[parentFrom] = parentTo; 
-		}
-	}
-
-
-	static class Edge implements Comparable<Edge>{
-		int from;
-		int to;
-		int weight;
+	public static boolean union(int a, int b) {
+		int parentA = find(a);
+		int parentB = find(b);
 		
-		public Edge(int from, int to, int weight) {
-			super();
-			this.from = from;
-			this.to = to;
-			this.weight = weight;
+		if(parentA == parentB) {
+			return false;
+		}else {
+//			System.out.println(a + " " + b);
+			if(parentA < parentB) {
+				check[parentB] = parentA;
+			}else {
+				check[parentA] = parentB;
+			}
+			return true;
 		}
-
-		@Override
-		public int compareTo(Edge o) {
-			return Integer.compare(this.weight, o.weight);
+	}
+	
+	public static class Bridge implements Comparable<Bridge>{
+		int start;
+		int dest;
+		int len;
+		
+		public Bridge(int start, int dest, int len) {
+			this.start = start;
+			this.dest = dest;
+			this.len = len;
 		}
-
+		
 		@Override
 		public String toString() {
-			return "Edge [from=" + from + ", to=" + to + ", weight=" + weight + "]";
+			return start + " " + dest + " " + len;
 		}
 		
+		@Override
+		public int compareTo(Bridge o) {
+			return Integer.compare(this.len, o.len);
+		}
 	}
-
-	private static void bfs(int[] start) {
-		Deque<int[]> dq = new LinkedList<int[]>();
-		dq.offer(start);
-		visited[start[0]][start[1]] = true;
-		field[start[0]][start[1]] = landIdx;
+	
+	public static void makeBridge(int start, int[] node) {
+		for(int dir=0; dir<4; dir++) {
+			int ar = node[0];
+			int ac = node[1];
+			int len = 0;
+			
+			while(true) {
+				ar += dr[dir];
+				ac += dc[dir];
+				
+				if(ar >= N || ar < 0 || ac >= M || ac < 0) {
+					break;
+				}
+				
+				if(field[ar][ac] == 0) {
+					len += 1;
+				}else {
+					if(len >= 2) {
+						int dest = field[ar][ac];
+						
+						pq.offer(new Bridge(start, dest, len));
+					}
+					break;
+				}
+			}
+		}
+	}
+	
+	public static void bfs(int[] node) {
+		Deque<int[]> dq = new LinkedList<>();
+		field[node[0]][node[1]] = landIdx;
+		visited[node[0]][node[1]] = true;
+		dq.offer(node);
 		
 		while(!dq.isEmpty()) {
 			int[] current = dq.poll();
-			int r = current[0];
-			int c = current[1];
 			
 			for(int dir=0; dir<4; dir++) {
-				int ar = r + dr[dir];
-				int ac = c + dc[dir];
+				int ar = current[0] + dr[dir];
+				int ac = current[1] + dc[dir];
 				
-				if(ar < 0 || ar >= N || ac < 0 || ac >= M || visited[ar][ac] || field[ar][ac] == 0) {
+				if(ar >= N || ar < 0 || ac >= M || ac < 0 || visited[ar][ac] || field[ar][ac] == 0) {
 					continue;
 				}
 				
+				field[ar][ac] = landIdx;
 				visited[ar][ac] = true;
 				dq.offer(new int[] {ar, ac});
-				field[ar][ac] = landIdx;
 			}
 		}
-		landIdx++;
-	} 
+		
+		landIdx += 1;
+	}
 }
